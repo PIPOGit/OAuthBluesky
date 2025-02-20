c98d1adf899ea377b00379a93248676cc2c03574
user.email <ArturoGarciaMartin@gmail.com>
2025-02-20 17:28:33 +0100

    Updated to v.1.6.0
    + Improved renedering of tables.
    + Added icons to record elements in tables.

--- js/modules/common.types.js
+++ js/modules/common.types.js
@@ -69,6 +69,7 @@ export class APICallError extends Error {
 		message: "",
 	};
 */
+
 export class AccessTokenError extends Error {
 	title								= "Access Token Error";
 	message								= null;
@@ -118,7 +119,25 @@ export class DPoPRequest {
 	}
 }
 
-
+export class APICallResponseError extends Error {
+	error								= null;
+	step								= null;
+	callbackOptions						= null;
+	sameSteps							= null;
+	currentStep							= null;
+	distinctDPoPNonce					= null;
+	serverError							= null;
+	constructor( error, step, callbackOptions, sameSteps, currentStep, distinctDPoPNonce, serverError ) {
+		super( "Error @ " + step );
+		this.error						= error;
+		this.step						= step;
+		this.callbackOptions			= callbackOptions;
+		this.sameSteps					= sameSteps;
+		this.currentStep				= currentStep;
+		this.distinctDPoPNonce			= distinctDPoPNonce;
+		this.serverError				= serverError;
+	};
+}
 /**********************************************************
  * Module Variables
  **********************************************************/
